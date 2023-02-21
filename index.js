4df1bdfc922b75f7e25cbb8064ddda4f57a695e6
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 19:28:32 +0300

    heroku

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-import express from "express"
+const express = require("express")
+
 const app = express()
-
-var cors = require('cors')
-bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
 app.use(express.json())
 
 
-
-const db = require("./data/db")
-app.use(express.json())
-
-
-
-
-
-app.get("/", async (req, res) => {
-    res.send("Test")
+app.get("/",(req,res) =>{
+    console.log("Test");
 })
 
- 
-app.get("/test", async (req, res) => {
-
-    try {
-        let result = await db.execute("SELECT * FROM user")
-        res.json(result[0])
-
-    } catch (err) {
-        console.log(err);
-    }
-})
-
-
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log("server ")
-})
+app.listen(process.env.PORT || 5000)
