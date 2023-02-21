597a8c0a6586dc9d8ae6a61b118f5536d64e52bf
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 19:42:38 +0300

    adfasdf

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express"
 const app = express()
-
-
-
-var cors = require("cors")
+app.use(express.json())
+import cors from "cors"
 app.use(cors())
-bodyParser = require('body-parser');
+
+import bodyParser from "body-parser"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json())
+
 
 app.get("/", (req, res) => {
     res.send("get")
 })
 
-app.listen(process.env.PORT || 5000)
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("test")
+})
